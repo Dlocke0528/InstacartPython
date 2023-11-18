645815028ed9895badb22c5e7a6fb82a58b69730
Dlocke0528 <151388177+Dlocke0528@users.noreply.github.com>
2023-11-18 10:51:56 -0800

    Add files via upload

---
+++ 4.10 Final Project/2. Scripts and Notebooks/4.2 Importing libraries and Python data types (1).py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+# #01. import libraries
+
+# In[3]:
+
+
+110-10
+
+
+# In[4]:
+
+
+x=25
+
+
+# In[5]:
+
+
+b=75
+
+
+# In[6]:
+
+
+x+b
+
+
+# In[7]:
+
+
+print(x)
+
+
+# In[8]:
+
+
+x+75
+
+
+# In[9]:
+
+
+90+10
+
+
+# In[10]:
+
+
+x=7.85
+
+
+# In[11]:
+
+
+type(x)
+
+
+# In[12]:
+
+
+c=2.345
+
+
+# In[13]:
+
+
+type(c)
+
+
+# In[14]:
+
+
+x/c
+
+
+# In[15]:
+
+
+7.85/2.345
+
+
+# In[16]:
+
+
+a=4.69
+
+
+# In[17]:
+
+
+b=9.56
+
+
+# In[18]:
+
+
+a/b
+
+
+# In[22]:
+
+
+d='good morning,'
+
+
+# In[24]:
+
+
+e= 'charlie'
+
+
+# In[25]:
+
+
+d+e
+
+
+# In[31]:
+
+
+n='tomato'
+
+
+# In[32]:
+
+
+n*12
+
+
+# In[38]:
+
+
+g = 'have a '
+
+
+# In[47]:
+
+
+h = 'great'
+
+
+# In[56]:
+
+
+j =  'weekend!'
+
+
+# In[57]:
+
+
+g+h+j
+
+
+# In[55]:
+
+
+m= 'You'
+
+
+# In[67]:
+
+
+p= ' remind me '
+
+
+# In[71]:
+
+
+T= 'of my old '
+
+
+# In[72]:
+
+
+z= 'classmate'
+
+
+# In[73]:
+
+
+m+p+T+z
+
+
+# In[ ]:
+
+
+
+
